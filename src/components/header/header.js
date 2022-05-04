ddcdb2ab60f966c6e0494ae5ad9d4e52a93512b8
MistbornL <lashaloria3@gmail.com>
2022-05-04 12:11:12 +0400

    added head middle part

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,22 +3,37 @@ import Logo from '../logo'
 import Search from '../search'
 import "./index.css"
 import Nav from '../navigation/nav'
-
+import moonstatue from '../../ui/moonstatue.png'
 
 const Header = () => {
   return (
-    <header style={{width: '100%', display: 'flex', alignItems: 'center'}}>
+    <header style={{width: '100%',  height: '739px'}}>
        
         <div className="container">
             <div style={{display: 'flex', alignItems: 'center'}}>
                 <Logo />
                 <Search /> 
             </div>
-            <Nav/>
-            <div className="btn">
-                <button type="submit" value="Submit">Connect Wallet</button>
+
+            <div className = "futa">
+                <Nav/>
+                <div className="btn">
+                    <button type="submit" value="Submit">Connect Wallet</button>
+                </div>
             </div>
         </div>
+
+        <div className="middle_head">
+            <div className="artworks">
+                <p>Collect Super Rare Digital <span>Artworks</span></p>
+            </div>
+
+            <div className="moon-statue">
+                <img src={moonstatue} alt="statue"/> 
+            </div>
+            
+        </div>
+        
     </header>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,14 @@
 import Header from "../components/header/header";
 import "./index.css"
-import moon from "../ui/moon.png"
-import statue from "../ui/statue.png"
+
+
 const Home = () => {
     return (
         <div className="wrap">
             <Header/>
-        <div className="line"></div>      
+            <div className="line"></div>      
         </div>
+
     )
 }
 
