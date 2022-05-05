a4581c33c2e5baf145c8b22f4304c82405297cc0
MistbornL <lashaloria3@gmail.com>
2022-05-05 12:01:45 +0400

    fixed div

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,30 +6,25 @@ import Nav from '../navigation/nav'
 import Middle from './middle_head/middle'
 
 const Header = () => {
-  return (
-    <header style={{width: '100%',  height: '739px'}}>
-       
-        <div className="container">
-            <div style={{display: 'flex', alignItems: 'center'}}>
-                <Logo />
-                <Search /> 
-            </div>
-
-            <div className = "futa">
-                <Nav/>
-                <div className="btn">
-                    <button type="submit" value="Submit">Connect Wallet</button>
+    return (
+        <header style={{ width: '100%', height: '739px' }}>
+            <div className="container">
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Logo />
+                    <Search />
                 </div>
 
-            </div>
-        </div>
+                <div className="futa">
+                    <Nav />
+                    <div className="btn">
+                        <button type="submit" value="Submit">Connect Wallet</button>
+                    </div>
 
-        <Middle />
-        
-        
-        
-    </header>
-  )
+                </div>
+            </div>
+            <Middle />
+        </header>
+    )
 }
 
 export default Header
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import "./index.css"
+
+
 function Main() {
-  return (
-    <div className="wrapper">
-        <div className="popular">
-            <h1>Most <span>Popular</span></h1>
+    return (
+        <div className="wrapper">
+            <div className="popular">
+                <h1>Most <span>Popular</span></h1>
                 <button className='view_all'>
                     View All
                 </button>
-        </div>
+            </div>
 
-        <section className =''>
-            <div></div>
-        </section>
-    </div>
-  )
+            <section className=''>
+                <div></div>
+            </section>
+        </div>
+    )
 }
 
 export default Main
