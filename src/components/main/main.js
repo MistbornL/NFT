dd3ec243c3c98e741fb60d708185d18d1a1f6c4e
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-05-07 14:41:53 +0400

    update

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -1,45 +1,49 @@
-import React from 'react'
-import "./index.css"
-import Popular from './Popular/Popular'
+import React from "react";
+import "./index.css";
+import Popular from "./Popular/Popular";
 
-
-import NtfCard from './nftCard/NtfCard'
-import NFT_CARDS from '../../dummyData/data'
-import { TopSellers } from './top_sellers_section/TopSellers'
-import Data from '../../dummyData/second_data'
-
+import NtfCard from "./nftCard/NtfCard";
+import NFT_CARDS from "../../dummyData/data";
+import { TopSellers } from "./top_sellers_section/TopSellers";
+import Data from "../../dummyData/second_data";
 
 function Main({ item }) {
-    return (
-        <div className="wrapper">
-            <Popular />
-            <section style={{ display: 'flex', justifyContent: 'space-around', marginBottom: '73px' }}>
-                {
-                    NFT_CARDS.map(item => (
-                        <NtfCard item={item} key={item.id} />
-                    ))
-                }
-            </section>
+  return (
+    <div className="wrapper">
+      <Popular />
+      <section
+        style={{
+          display: "flex",
+          justifyContent: "space-around",
+          marginBottom: "73px",
+        }}
+      >
+        {NFT_CARDS.map((item) => (
+          <NtfCard item={item} key={item.id} />
+        ))}
+      </section>
 
-            <div className='popular'>
-                <h1>Top <span>Sellers</span></h1>
-                <button className='view_all'>
-                    View All
-                </button>
-            </div>
+      <div className="popular">
+        <h1>
+          Top <span>Sellers</span>
+        </h1>
+        <button className="view_all">View All</button>
+      </div>
 
-            <section className='middleel'>
-                {
-                    Data.map(item => (
-                        <TopSellers item={item} key={item.id} />
-                    ))
-                }
+      <section className="middleel">
+        {Data.map((item) => (
+          <TopSellers item={item} key={item.id} />
+        ))}
+      </section>
 
-            </section>
-
-
-        </div>
-    )
+      <div className="popular">
+        <h1>
+          About <span>Cube</span>
+        </h1>
+        <button className="view_all">View All</button>
+      </div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
