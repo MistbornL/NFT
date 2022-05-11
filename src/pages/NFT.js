6b4dd403ffb7469b2ffc2dca800629580ee192d7
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-05-11 21:57:20 +0400

    hou

--- src/pages/NFT.js
+++ src/pages/NFT.js
@@ -1,97 +1,145 @@
-import React, { useState, useEffect } from 'react'
-import NFT_CARDS from '../dummyData/data';
+import React, { useState, useEffect } from "react";
+import NFT_CARDS from "../dummyData/data";
 import { useParams } from "react-router-dom";
-import '../components/header/index.css'
-import { Menu } from '../components/header/Menu';
+import "../components/header/index.css";
+import { Menu } from "../components/header/Menu";
 
 export const NFT = () => {
-    const params = useParams();
-    console.log(params)
+  const params = useParams();
+  console.log(params);
 
-    const [singleCard, setSingleCard] = useState();
+  const [singleCard, setSingleCard] = useState();
 
-    useEffect(() => {
-        let x = NFT_CARDS.filter(c => c.id == params.id)[0];
-        setSingleCard(x)
-    }, [])
+  useEffect(() => {
+    let x = NFT_CARDS.filter((c) => c.id == params.id)[0];
+    setSingleCard(x);
+  }, []);
 
+  console.log(singleCard);
 
-    console.log(singleCard)
+  return (
+    <div className="wrap">
+      <Menu />
+      <div className="line" />
 
-    return (
-        <div className="wrap">
-            <Menu />
-            <div className='line' />
+      <div
+        style={{
+          display: "flex",
+          width: "448px",
+          height: "445px",
+          marginTop: "79px",
+        }}
+      >
+        {/* <img style={{ width: "100%" }} src={singleCard.coverImg}></img> */}
+      </div>
 
-            <div style={{ display: 'flex', width: '448px', height: '445px', marginTop: '79px' }}>
-                <img style={{ width: '100%' }} src={singleCard.coverImg}></img>
-            </div>
+      <div className="description-card">
+        <div className="description-top">
+          <h1></h1>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <p></p>
+        </div>
+        <div className="line-for-description-card"></div>
 
-            <div className='description'>
-                <h1>Description</h1>
-                <div className="line-for-div"></div>
-                <div className='scroll'>
-                    <p>We can predict in advance the main argument of the critics - a housewife cannot be considered a real professional in each of the listed areas! But we do not argue with this, because the activities of a housewife include the skills of almost all of the listed professions. But that's not all - a housewife is a mother who tries her best to raise future worthy members of society, and does it on her own, without resorting to the help of surrogate mothers, whose services cost from 25 thousand dollars. Who, if not the state itself, should devote more time to them, protect their rights and, in the end, start playing for their hard work?! We urge you to respect and appreciate</p>
-                </div>
-
-            </div>
-
-            <div className='description'>
-                <h1>Properties</h1>
-                <div className="line-for-div"></div>
-                <div className='div-wrapper'>
-                    <div className='small-div'>
-                        <p></p>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-                    <div className='small-div'>
-                        <p></p>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-                    <div className='small-div'>
-                        <p></p>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-
-                    <div className='small-div'>
-                        <p></p>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-
-                    <div className='small-div'>
-                        <p></p>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-
-                    <div className='small-div'>
-                        <p></p>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-                    <div className='small-div'>
-                        <p></p>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-                    <div className='small-div'>
-                        <p></p>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-                    <div className='small-div'>
-                        <p></p>
-                        <h1></h1>
-                        <p></p>
-                    </div>
-                </div>
-
-            </div>
+        <div className="bid-countdown">
+          <p></p>
+          <p></p>
         </div>
 
-    )
-}
+        <div className="description-main">
+          <img></img>
+          <div>
+            <h1>
+              <span></span>
+            </h1>
+            <p></p>
+          </div>
+        </div>
+
+        <div className="description-footer">
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+
+      <div className="description">
+        <h1>Description</h1>
+        <div className="line-for-div"></div>
+        <div className="scroll">
+          <p>
+            We can predict in advance the main argument of the critics - a
+            housewife cannot be considered a real professional in each of the
+            listed areas! But we do not argue with this, because the activities
+            of a housewife include the skills of almost all of the listed
+            professions. But that's not all - a housewife is a mother who tries
+            her best to raise future worthy members of society, and does it on
+            her own, without resorting to the help of surrogate mothers, whose
+            services cost from 25 thousand dollars. Who, if not the state
+            itself, should devote more time to them, protect their rights and,
+            in the end, start playing for their hard work?! We urge you to
+            respect and appreciate
+          </p>
+        </div>
+      </div>
+
+      <div className="description">
+        <h1>Properties</h1>
+        <div className="line-for-div"></div>
+        <div className="div-wrapper">
+          <div className="small-div">
+            <p></p>
+            <h1></h1>
+            <p></p>
+          </div>
+          <div className="small-div">
+            <p></p>
+            <h1></h1>
+            <p></p>
+          </div>
+          <div className="small-div">
+            <p></p>
+            <h1></h1>
+            <p></p>
+          </div>
+
+          <div className="small-div">
+            <p></p>
+            <h1></h1>
+            <p></p>
+          </div>
+
+          <div className="small-div">
+            <p></p>
+            <h1></h1>
+            <p></p>
+          </div>
+
+          <div className="small-div">
+            <p></p>
+            <h1></h1>
+            <p></p>
+          </div>
+          <div className="small-div">
+            <p></p>
+            <h1></h1>
+            <p></p>
+          </div>
+          <div className="small-div">
+            <p></p>
+            <h1></h1>
+            <p></p>
+          </div>
+          <div className="small-div">
+            <p></p>
+            <h1></h1>
+            <p></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
