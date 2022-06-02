4bd47d830f9664c2d4932e1680d27a75a2b655f4
Lasha <50033103+MistbornL@users.noreply.github.com>
2022-06-02 17:27:24 +0400

    sass

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./popup.scss";
+import cross from "../../ui/cross.svg";
+import { useDispatch } from "react-redux";
+import { renderPopUp } from "../../state/actions";
 export const Popup = () => {
+  const dispatch = useDispatch();
   return (
     <div className="wallet">
+      <img
+        onClick={() => {
+          dispatch(renderPopUp(false));
+        }}
+        src={cross}
+        alt="cross"
+      />
       <h1>Connect Your Wallet</h1>
       <p>
         Choose one of available wallet providers or create a new wallet. What is
